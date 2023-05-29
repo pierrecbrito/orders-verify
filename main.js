441e721e5f837b30e12bfe783bce050b28f37cfb
Pierre Brito <carlospierre07@gmail.com>
2023-05-29 14:14:46 -0300

    Preparando leitor

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 require('dotenv').config()
 
-const APP_KEY = process.env.APP_KEY
+const USER_KEY = process.env.USER_KEY
+const ENDPOINT = "https://api2.ploomes.com/Orders?$expand=Products"
+
+const options = {
+    method: "get",
+    body: {},
+    headers: { 
+        "Content-Type": "application/json",
+        "User-Key" : USER_KEY
+    }
+    
+}
+
+/**
+ * @returns - Um array com produtos ({nome, codigo, valor})
+ */
+async function ler() {
+    let dados = []
+
+
+
+    const response = await fetch(ENDPOINT, options);
+    const responseJson = await response.json();
+
+    console.log(responseJson.value)
+    
+    
+    return dados
+}
